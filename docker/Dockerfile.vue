4f55997400960dc6e7204a3cf999359aca158d47
Afshin Tavakolian <afsh7n@gmail.com>
2024-09-28 02:18:02 +0330

    update docker files

---
+++ docker/Dockerfile.vue
@@ -0,0 +1,14 @@
+# Dockerfile for Vue Project
+FROM node:16-alpine
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+RUN npm run build
+
+RUN npm install -g serve
+EXPOSE 8080
+CMD ["serve", "-s", "dist"]
--- setup.sh
+++ setup.sh
@@ -13,10 +13,6 @@ project_directory="/home/deployer/automated-server-setup-front"
 echo -e "${BLUE}Cloning project structure into $project_directory...${NC}"
 if [ -d "$project_directory" ]; then
     echo -e "${GREEN}Project structure already exists. Pulling the latest changes.${NC}"
-
-    # Adding the directory to Git's safe.directory list
-    sudo -u $deploy_user git config --global --add safe.directory $project_directory
-
     cd $project_directory && sudo -u $deploy_user git pull
 else
     git clone $project_structure_url $project_directory
@@ -33,24 +29,20 @@ else
     echo -e "${BLUE}Creating user '$deploy_user'...${NC}"
     sudo adduser --disabled-password --gecos "" $deploy_user
     sudo usermod -aG sudo $deploy_user
-    echo -e "${GREEN}User '$deploy_user' created and added to sudo group.${NC}."
+    echo -e "${GREEN}User '$deploy_user' created and added to sudo group.${NC}"
 fi
 
 # Step 3: Generate SSH key for deploy_user and root (if needed)
 if [ -f "/home/$deploy_user/.ssh/id_rsa.pub" ]; then
     echo -e "${GREEN}SSH key for '$deploy_user' already exists. Skipping SSH key generation.${NC}"
 else
-    # Ensure the home directory and .ssh directory exist and are owned by deploy_user
-    echo -e "${BLUE}Ensuring home directory and .ssh directory for $deploy_user...${NC}"
+    echo -e "${BLUE}Generating SSH key for $deploy_user...${NC}"
     sudo mkdir -p /home/$deploy_user/.ssh
     sudo chown -R $deploy_user:$deploy_user /home/$deploy_user
     sudo chmod 700 /home/$deploy_user/.ssh
 
-    # Generate SSH key for the deploy user
-    echo -e "${BLUE}Generating SSH key for $deploy_user...${NC}"
     sudo -u $deploy_user ssh-keygen -t rsa -b 4096 -C "exp@exp.com" -N "" -f /home/$deploy_user/.ssh/id_rsa
 
-    # Ensure the permissions for the keys are correct
     sudo chmod 600 /home/$deploy_user/.ssh/id_rsa
     sudo chmod 644 /home/$deploy_user/.ssh/id_rsa.pub
 
@@ -99,12 +91,6 @@ for project_name in "${!project_folders[@]}"; do
         echo -e "${GREEN}Folder removed successfully.${NC}"
     fi
 
-    # Create folder after removal
-    if [ ! -d "$folder_path" ]; then
-        echo -e "${BLUE}Creating folder $folder_name...${NC}"
-        mkdir -p $folder_path
-    fi
-
     # Clone the repository
     echo -e "${BLUE}Cloning $project_name into $folder_path...${NC}"
     git clone $repo_url $folder_path
@@ -130,66 +116,20 @@ for project_name in "${!project_folders[@]}"; do
             echo -e "${RED}vite.config.js not found for $project_name. Skipping base configuration.${NC}"
         fi
 
-        # Check if Dockerfile exists, if not, create it for React, Vue, and Docs projects
-        if [ ! -f "$folder_path/Dockerfile" ] && ([[ "$project_name" == "onomis-react" ]] || [[ "$project_name" == "onomis-vue" ]] || [[ "$project_name" == "onomis-docs" ]]); then
-            echo -e "${RED}No Dockerfile found for $project_name. Adding a default Dockerfile...${NC}"
-
-            if [ "$project_name" == "onomis-react" ]; then
-                # Add React Dockerfile
-                cat <<EOL > "$folder_path/Dockerfile"
-# Dockerfile for React Project
-FROM node:16-alpine
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
-COPY . .
-RUN npm run build
-RUN npm install -g serve
-EXPOSE 3000
-CMD ["serve", "-s", "dist"]
-EOL
-
-            elif [ "$project_name" == "onomis-vue" ]; then
-                # Add Vue Dockerfile
-                cat <<EOL > "$folder_path/Dockerfile"
-# Dockerfile for Vue Project
-FROM node:16-alpine
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
-COPY . .
-RUN npm run build
-RUN npm install -g serve
-EXPOSE 8080
-CMD ["serve", "-s", "dist"]
-EOL
-
-            elif [ "$project_name" == "onomis-docs" ]; then
-                # Add Docs Dockerfile
-                cat <<EOL > "$folder_path/Dockerfile"
-# Dockerfile for Onomis Docs Project
-FROM node:16-alpine
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
-COPY . .
-RUN npm run docs:build
-RUN npm install -g serve
-EXPOSE 3000
-CMD ["serve", "-s", ".vitepress/dist"]
-EOL
-            fi
+        # Check if package.json exists, and ensure dependencies are installed
+        package_json_path="$folder_path/package.json"
+        if [ -f "$package_json_path" ]; then
+            echo -e "${BLUE}Installing dependencies for $project_name...${NC}"
+            sudo -u $deploy_user npm install --prefix $folder_path
+            echo -e "${GREEN}Dependencies installed successfully for $project_name.${NC}"
+        else
+            echo -e "${RED}package.json not found for $project_name. Skipping dependency installation.${NC}"
         fi
-
     else
         echo -e "${RED}Failed to clone $project_name. Please check the URL and SSH key.${NC}"
         exit 1
     fi
 done
-
-
-
-
 
 # Step 5: Install Docker (if needed)
 if command -v docker &>/dev/null; then
@@ -229,46 +169,7 @@ else
     echo -e "${GREEN}SSH port changed to 23232 and service restarted.${NC}"
 fi
 
-# Step 9: Install and configure GitLab Runner (if needed)
-if command -v gitlab-runner >/dev/null 2>&1; then
-    echo -e "${GREEN}GitLab Runner is already installed. Skipping installation.${NC}"
-else
-    echo -e "${BLUE}Installing GitLab Runner...${NC}"
-    curl -L --output /tmp/gitlab-runner-linux-amd64 "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
-    sudo mv /tmp/gitlab-runner-linux-amd64 /usr/local/bin/gitlab-runner
-    sudo chmod +x /usr/local/bin/gitlab-runner
-    echo -e "${GREEN}GitLab Runner installed successfully.${NC}"
-fi
-
-if sudo systemctl is-active --quiet gitlab-runner; then
-    echo -e "${GREEN}GitLab Runner is already running. Skipping this step.${NC}"
-else
-    echo -e "${BLUE}Installing and starting GitLab Runner as a service...${NC}"
-    sudo gitlab-runner install --user=$deploy_user --working-directory=/home/$deploy_user
-    sudo gitlab-runner start
-
-    echo -e "${BLUE}Please enter your GitLab Runner registration token:${NC}"
-    read registration_token
-
-    sudo gitlab-runner register --non-interactive \
-      --url "https://gitlab.com/" \
-      --registration-token "$registration_token" \
-      --executor "shell" \
-      --description "My Server Runner" \
-      --tag-list "server" \
-      --run-untagged="true" \
-      --locked="false"
-
-    sudo systemctl restart gitlab-runner
-    if sudo systemctl is-active --quiet gitlab-runner; then
-        echo -e "${GREEN}GitLab Runner is running and ready to accept jobs.${NC}"
-    else
-        echo -e "${RED}GitLab Runner failed to start after registration. Please check the logs for more details.${NC}"
-        exit 1
-    fi
-fi
-
-# Step 10: Start Docker Compose
+# Step 9: Start Docker Compose
 echo -e "${BLUE}Starting Docker Compose...${NC}"
 cd /home/$deploy_user/automated-server-setup-front
 docker-compose up -d --build
