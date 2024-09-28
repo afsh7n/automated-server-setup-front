abe86758511200090bdcd26de5e684405fe0499f
Afshin Tavakolian <afsh7n@gmail.com>
2024-09-28 04:07:01 +0330

    update docker files

--- docker/Dockerfile.vue
+++ docker/Dockerfile.vue
@@ -1,12 +1,18 @@
 # Dockerfile for Vue Project
-FROM node:16-alpine AS builder
+FROM node:16-alpine
 
 WORKDIR /usr/src/app
 
 COPY ./../src/onomis-vue/package*.json ./
+
 RUN npm install
 
-COPY ./../src/onomis-vue ./
+COPY ./../src/onomis-vue/. .
+
 RUN npm run build
 
-# No final Nginx setup, just build and leave the output
+RUN npm install -g serve
+
+EXPOSE 3001
+
+CMD ["serve", "-s", "dist", "-l", "3001"]
