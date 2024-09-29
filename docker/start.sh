b9bbf5d754fadde203c132e1134d83b9dc5ef5a7
Afshin Tavakolian <afsh7n@gmail.com>
2024-09-29 03:35:49 +0330

    last update docker files

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-echo "Starting serve on port ${PORT}"
-serve -s dist -l tcp://0.0.0.0:${PORT}
+# Check if PORT is set, otherwise set to default
+if [ -z "$PORT" ]; then
+  PORT=3000
+fi
+
+# Check if FOLDER_NAME is set, otherwise default to "dist"
+if [ -z "$FOLDER_NAME" ]; then
+  FOLDER_NAME="dist"
+fi
+
+# Echo the port and folder name for debugging
+echo "Starting serve on port ${PORT} and serving folder ${FOLDER_NAME}"
+
+# Serve the folder on the specified port
+serve -s $FOLDER_NAME -l tcp://0.0.0.0:$PORT
